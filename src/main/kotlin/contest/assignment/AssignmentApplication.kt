c1bf9e52200335172c8457ca05d525a634618066
김준환 <musoyou1085@gmail.com>
2025-07-16 16:59:53 +0900

    chore: JPAAuditing 추가

--- src/main/kotlin/contest/assignment/AssignmentApplication.kt
+++ src/main/kotlin/contest/assignment/AssignmentApplication.kt
@@ -2,8 +2,10 @@ package contest.assignment
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class AssignmentApplication
 
 fun main(args: Array<String>) {
